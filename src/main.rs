ea74a01697472b98d8972c8d1f1483140aa3e6f1
Adam Hammes <ahammes@cortexmedia.ca>
2018-03-09 11:16:43 -0500

    Able to receive private messages

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,43 @@
+extern crate slack;
+use slack::{Event, RtmClient, Message};
+
+extern crate dotenv;
+use dotenv::dotenv;
+use std::env;
+
+struct MessageHandler;
+
+#[allow(unused_variables)]
+impl slack::EventHandler for MessageHandler {
+    fn on_event(&mut self, cli: &RtmClient, event: Event) {
+        match event {
+            Event::Message(msg_event) => {
+                if let Message::Standard(msg) = *msg_event {
+                    println!("{:?}", msg.text);
+                    println!("{:?}", msg.user);
+                }
+            },
+            _ => {}
+        }
+    }
+
+    fn on_close(&mut self, cli: &RtmClient) {
+    }
+
+    fn on_connect(&mut self, cli: &RtmClient) {
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    dotenv().ok();
+    let api_key = env::var("BOT_ACCESS_TOKEN")
+        .expect("Couldn't read environment variable BOT_ACCESS_TOKEN");    
+
+    let mut handler = MessageHandler;
+    let r = RtmClient::login_and_run(&api_key, &mut handler);
+
+    match r {
+        Ok(_) => {}
+        Err(err) => panic!("Error: {}", err),
+    }
 }
