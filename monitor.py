b337abc2a0d9f9c36cf96a8af30e7f2932205061
Adam Hammes <ahammes@cortexmedia.ca>
2018-04-13 10:27:38 -0400

    Print time when restarting

--- monitor.py
+++ monitor.py
@@ -1,9 +1,11 @@
+from datetime import datetime
 from subprocess import Popen, TimeoutExpired
 
 PROCESS_TIMEOUT = 30 * 60
 
 def main():
 	while True:
+		print(str(datetime.now()))
 		bot_process = Popen('cargo run', shell=True)
 		try:
 			bot_process.wait(timeout=PROCESS_TIMEOUT)
